6fd26aab09018c5a2cdec53968f0b1ead4040cff
kushal <shahkushal278@gmail.com>
2024-01-20 18:58:30 -0500

    Initial Commit

---
+++ Gitexercise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gitexercise
+//
+//  Created by Kushal shah on 2024-01-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gitexercise/GitexerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitexerciseApp.swift
+//  Gitexercise
+//
+//  Created by Kushal shah on 2024-01-20.
+//
+
+import SwiftUI
+
+@main
+struct GitexerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
