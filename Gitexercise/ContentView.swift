0e8b71594a47c28f94eec6e44fd732b4e8e237cd
kushal <shahkushal278@gmail.com>
2024-01-20 19:41:20 -0500

    version 4.0

--- Gitexercise/ContentView.swift
+++ Gitexercise/ContentView.swift
@@ -13,13 +13,13 @@ struct ContentView: View {
             Image(systemName: "3.circle")
                 .resizable()
                 .scaledToFit()
-            Text("Version 3.0")
+            Text("Version 4.0")
                 .font(.largeTitle)
                 .fontWeight(.black)
             Rectangle()
         }
         .padding()
-        .foregroundColor(.blue)
+        .foregroundColor(.purple)
     }
 }
 
